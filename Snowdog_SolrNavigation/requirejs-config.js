12a48f16c8d8bf06a8f7c9b2d2c7ef4e1069a88d
sickdaflip <philippbreitsprecher@gmail.com>
2020-03-23 15:45:19 +0100

    first upload

---
+++ Snowdog_SolrNavigation/requirejs-config.js
@@ -0,0 +1,11 @@
+var config = {
+  map: {
+    '*': {
+      'priceRange': 'Snowdog_SolrNavigation/js/price-range.babel',
+      'quickSearch': 'Snowdog_SolrNavigation/js/quicksearch.babel',
+      'range': 'Snowdog_SolrNavigation/js/range.babel',
+      'toolbar': 'Snowdog_SolrNavigation/js/toolbar.babel',
+      'solrAddToCart': 'Snowdog_SolrNavigation/js/solr-add-to-cart'
+    }
+  }
+};
---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/Sickdaflip/rational',
+    __DIR__
+);
